9bd7e30c2a549835b86eda05fc70cd0104ff785c
ハナウタ三浦 <miura.hnut@gmail.com>
2021-01-19 08:20:14 +0900

    起動時の画面をMainTabControllerに変更

---
+++ iOS-UISample/MainTabController.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabController.swift
+//  iOS-UISample
+//
+//  Created by nagisa miura on 2021/01/19.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    // MARK: - Properties
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+    // MARK: - Selectors
+
+    // MARK: - API
+
+    // MARK: - Helpers
+}
