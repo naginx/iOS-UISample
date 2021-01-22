286eaeacbe45d040d78e26274bf3f019a8c3f6a0
ハナウタ三浦 <miura.hnut@gmail.com>
2021-01-23 08:48:40 +0900

    ユーザーの仮データと画像を追加

--- iOS-UISample/API/UserAPI.swift
+++ iOS-UISample/API/UserAPI.swift
@@ -27,33 +27,70 @@ final class UserAPI {
 }
 
 private extension UserAPI {
-    func userData() -> Data {
-        let json =
-            """
-            [
-                {
-                    "id": 1,
-                    "name": "太郎",
-                    "age": 20,
-                    "image": "suits_man",
-                    "message": "はじめまして。よろしくお願いします!!"
-                },
-                {
-                    "id": 2,
-                    "name": "花子",
-                    "age": 16,
-                    "image": "snow_woman",
-                    "message": "ヨロシク！"
-                },
-                {
-                    "id": 3,
-                    "name": "次郎",
-                    "age": 14,
-                    "image": "base_man",
-                    "message": "Hello. I'm Jiro."
-                }
-            ]
-            """
+
+    /// API呼び出しで返ってくることを想定したJSONデータ
+    /// 画像引用元: https://www.pakutaso.com/model.html
+    func userData() -> Data { // swiftlint:disable:this function_body_length
+        let json = """
+[
+    {
+        "id": 1,
+        "name": "太郎",
+        "age": 20,
+        "image": "suits_man",
+        "message": "はじめまして。よろしくお願いします!!"
+    },
+    {
+        "id": 2,
+        "name": "花子",
+        "age": 16,
+        "image": "snow_woman",
+        "message": "ヨロシク！"
+    },
+    {
+        "id": 3,
+        "name": "次郎",
+        "age": 14,
+        "image": "base_man",
+        "message": "Hello. I'm Jiro."
+    },
+    {
+        "id": 4,
+        "name": "梅子",
+        "age": 17,
+        "image": "christmas_girl",
+        "message": "こんにちは！"
+    },
+    {
+        "id": 5,
+        "name": "竹子",
+        "age": 18,
+        "image": "manager_girl",
+        "message": "暇だよー"
+    },
+    {
+        "id": 6,
+        "name": "松子",
+        "age": 18,
+        "image": "eat_girl",
+        "message": "お腹すいた..."
+    },
+    {
+        "id": 7,
+        "name": "炭治郎",
+        "age": 43,
+        "image": "tasogare_man",
+        "message": "寂しいです(泣)"
+    },
+    {
+        "id": 8,
+        "name": "君麻呂",
+        "age": 28,
+        "image": "battou_man",
+        "message": "どうも"
+    }
+]
+"""
         guard let jsonData = json.data(using: .utf8) else { return Data() }
         return jsonData
     }
