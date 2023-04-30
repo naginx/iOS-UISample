f9bd31e2101832a7b4d6487d914f73223601b6ad
nagisa miura <nagisa@miuranagisanoMacBook-Air.local>
2023-04-30 13:03:49 +0900

    Quick/Nimbleによるテストケースを追加

--- iOS-UISampleTests/CounterViewControllerSpec.swift
+++ iOS-UISampleTests/CounterViewControllerSpec.swift
@@ -4,4 +4,28 @@ import Nimble
 
 final class CounterViewControllerSpec: QuickSpec {
 
+    @MainActor
+    override func spec() {
+        var vc: CounterViewController!
+        beforeEach {
+            vc = CounterViewController()
+            vc.loadViewIfNeeded()
+        }
+
+        describe("初期表示") {
+            it("カウントが0であること") {
+                expect(vc.countLabel.text).to(equal("0"))
+            }
+        }
+
+        describe("+ボタンをタップ") {
+            context("現在値が0") {
+                it("カウンタが1に増えること") {
+                    vc.incrementButton.sendActions(for: .touchUpInside)
+                    expect(vc.countLabel.text).to(equal("1"))
+                }
+            }
+        }
+
+    }
 }
