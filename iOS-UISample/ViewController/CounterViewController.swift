e47082b20961a9378cddaf58f6c405f0759eb08f
nagisa miura <nagisa@miuranagisanoMacBook-Air.local>
2023-04-30 11:16:08 +0900

    BDD学習用のサンプルVCを追加

---
+++ iOS-UISample/ViewController/CounterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CounterViewController.swift
+//  iOS-UISample
+//
+//  Created by nagisa miura on 2023/04/30.
+//
+
+import UIKit
+
+final class CounterViewController: UIViewController {
+
+    @IBOutlet private weak var countLabel: UILabel!
+    @IBAction private func tappedDecrementButton(_ sender: UIButton) {
+    }
+    @IBAction private func tappedIncrementButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
